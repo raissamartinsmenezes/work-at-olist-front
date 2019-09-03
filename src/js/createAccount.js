fbe4613b87dfda2fe91a05db45a86e963a8c10be
raissamartinsmenezes <raissamartinsdemenezes>
2019-09-02 21:42:06 -0300

    init createAccount.js

---
+++ src/js/createAccount.js
@@ -0,0 +1,6 @@
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirmPassword');
+const submitButton = document.getElementById('submitButton');
+
