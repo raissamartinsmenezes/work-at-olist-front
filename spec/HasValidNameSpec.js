dc8f51008ee3ad4bae7275307197533f2d030428
raissamartinsmenezes <raissamartinsdemenezes>
2019-09-18 16:14:46 -0300

    refactor functions

--- spec/HasValidNameSpec.js
+++
@@ -1,8 +0,0 @@
-describe("hasValidName", function(){
-    var hasValidName = require("../lib/HasValidName")
-    it("should contain special characters", function(){
-        nameValue.currentValue = 'João % Paulo';
-        hasValidName()
-        expect(nameValue.isItValid).to.be.false
-    })
-})
